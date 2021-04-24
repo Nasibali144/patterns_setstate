ad4f78c260f63252927571f4c50a81b7f03ac23b
Nasibali Abdiyev <abdiyevnasibali@gmail.com>
2021-04-24 12:21:48 +0500

    Update 0.0.1

--- lib/main.dart
+++ lib/main.dart
@@ -3,7 +3,6 @@ import 'package:patterns_setstate/pages/create_page.dart';
 import 'package:patterns_setstate/pages/home_page.dart';
 import 'package:patterns_setstate/pages/update_page.dart';
 
-//har bir o'rgangan arxitekturani 3 martadan yoz 1. ustoz bilan, 2. va 3. o'zing 4. esa uga vazifa bo'sin shunda yaxshiroq o'rganasan
 
 void main() {
   runApp(MyApp());
